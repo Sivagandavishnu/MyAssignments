141127cab8d0606b55521665fbaa8a80b43a39a8
Sivagandavishnu <114829271+Sivagandavishnu@users.noreply.github.com>
2022-10-02 00:11:36 +0530

    Add files via upload

---
+++ Day1homeassign.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Day1homeassign {
+	
+	int number = 5;
+	
+	short factorial = 1;
+	
+	public static void main(String[] args) {
+		
+	int n = 5;
+    	
+	int fact = 1;
+	
+	for (int i = 1; i <= n; i++)
+	{ 
+	   fact = fact*i;
+		}
+	System.out.println("factorial of " + "number" + "is:"+fact);
+		
+	}}
